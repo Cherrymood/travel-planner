5a4366aab7efb612c24cca9fee5816722aba1b52
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-02 12:59:22 -0800

    Created browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+import Price from "./pages/Pricing";
+
 export default function App() {
-  return <dev>Hello</dev>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Price />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }

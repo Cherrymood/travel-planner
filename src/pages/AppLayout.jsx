6540daed5218973fea439d69113fe36b19a1e25c
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-03 13:26:17 -0800

    Added map component to app layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,12 @@
-import AppNav from "../components/AppNav";
 import Sidebar from "../components/SIdebar";
+import Map from "../components/Map";
 import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
+      <Map />
     </div>
   );
 }

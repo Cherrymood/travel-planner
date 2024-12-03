35547ff95951fd240dab1dacd1ae898b20d0e440
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-03 13:50:13 -0800

    Added nested routes and index route

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,19 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
+//matching url to ui pages works on client side, it will never be reloaded

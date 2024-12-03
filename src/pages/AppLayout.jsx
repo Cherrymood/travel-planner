75a69454251b2670a87c5199b8b01fa7bd773cfe
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-02 16:33:50 -0800

    Created AppLayout pages and css modules, global css

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,8 +1,9 @@
 import AppNav from "../components/AppNav";
+import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
   return (
-    <div>
+    <div className={styles.app}>
       <AppNav />
       <h1>LayOut</h1>
     </div>

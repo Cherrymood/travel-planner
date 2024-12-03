61984342adbf9c991282b9bf18407eb24bd105e1
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-02 16:18:35 -0800

    Set up home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom";
-import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
-export default function HomePage() {
+export default function Homepage() {
   return (
-    <div>
-      <PageNav />
-      <h1>Home Page</h1>
-      <Link to="app">AppLayOut </Link>
-    </div>
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          Travel Planner keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map designed to track your footsteps across every city you’ve
+          visited. This map becomes a living record of your adventures, ensuring
+          you never forget your incredible experiences. Share your journeys with
+          friends and showcase the many places you’ve wandered. Capture the
+          essence of your travels, preserve memories, and inspire others to
+          explore the world just like you have.
+        </h2>
+      </section>
+    </main>
   );
 }

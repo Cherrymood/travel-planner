1b9aa11e35f1c62fe3550240e4bff6ccb079439c
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-03 13:21:52 -0800

    Added side bar to app-layout

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,11 @@
 import AppNav from "../components/AppNav";
+import Sidebar from "../components/SIdebar";
 import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
-      <AppNav />
-      <h1>LayOut</h1>
+      <Sidebar />
     </div>
   );
 }

88cbf09524ee96899cc7883df44e6ec208651913
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-17 13:04:44 -0800

    Added routes to each city

--- server/routes/citiesRoutes.js
+++ server/routes/citiesRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import getCities from "../controllers/getCities.js";
+import { getCities, getCity } from "../controllers/getCities.js";
 
 const citiesRouter = express.Router();
 
 citiesRouter.get("/", getCities);
+citiesRouter.get("/:id", getCity);
 
 export default citiesRouter;

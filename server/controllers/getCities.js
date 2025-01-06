924387c6ed40c366dc12dd61f0e44cd246e86d68
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2025-01-06 10:33:56 -0800

    Context api (#6)
    * Added CitiesContext
    
    * Consuming the CitiesContext
    
    * Added routes to each city
    
    * Finishing the City view
    
    ---------
    
    Co-authored-by: Valeriia Horodnycha <valeriia.horodnycha@gmail.com>

--- server/controllers/getCities.js
+++ server/controllers/getCities.js
@@ -2,8 +2,31 @@ import data from "../cities.js";
 
 const { cities } = data;
 
-export default function getCities(req, res) {
-  res.json(cities);
-  console.log("Cookies: ", req.cookies);
-  console.log("Signed Cookies: ", req.signedCookies);
+async function getCities(req, res) {
+  try {
+    res.json(cities);
+    console.log("Cookies:", req.cookies);
+    console.log("Signed Cookies:", req.signedCookies);
+  } catch (error) {
+    console.error("Error fetching cities:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
+
+async function getCity(req, res) {
+  try {
+    const cityId = parseInt(req.params.id, 10); // Convert id from string to number
+    const city = cities.find((c) => c.id === cityId);
+
+    if (!city) {
+      return res.status(404).json({ message: "City not found" });
+    }
+
+    res.json(city);
+  } catch (error) {
+    console.error("Error fetching city:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export { getCities, getCity };

56b6e4d73f761baec61fff176037fdac57f80f31
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2025-01-06 10:33:03 -0800

    App layout (#4)
    * Created pages
    
    * Created browser router
    
    * Created page not found
    
    * Created nav-bar
    
    * Used css modules
    
    * Set up home page
    
    * Set up product page
    
    * Created AppLayout pages and css modules, global css
    
    * Created global css
    
    * Created pages
    
    * Added Login page and added nav bar to each page
    
    * Added starter components
    
    * Added logo
    
    * Created Footer
    
    * Added side bar to app-layout
    
    * Added map component to app layout
    
    * Added nested routes and index route
    
    * Adde cityList and CityItem
    
    * Added Countries List
    
    * Added query string
    
    * Added useNavigate
    
    ---------
    
    Co-authored-by: Valeriia Horodnycha <valeriia.horodnycha@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,62 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import CountriesList from "./components/CountriesList";
+import City from "./components/City";
+import Form from "./components/Form";
+
+const BASE_URL = `http://localhost:3000`;
 
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<Navigate replace to="cities" />} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountriesList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="form" element={<Form />} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
 }
+//matching url to ui pages works on client side, it will never be reloaded
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,14 @@
+
+import Sidebar from "../components/SIdebar";
+import Map from "../components/Map";
 import AppNav from "../components/AppNav";
 import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
+      <Sidebar />
+      <Map />
       <AppNav />
       <h1>LayOut</h1>
     </div>

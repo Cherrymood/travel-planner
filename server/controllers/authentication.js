be17f3ed328549513e7fddb84c5eed82401dffc6
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2025-01-14 10:47:45 -0800

    Fixed name

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -5,7 +5,9 @@ async function register(req, res) {
   const user = await User.create({ ...req.body });
   const token = user.createJWT();
 
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { name: user.username }, token });
 }
 
 async function login(req, res) {
@@ -13,7 +15,7 @@ async function login(req, res) {
     const user = await User.checkExistUser(req.body);
     const token = user.createJWT();
 
-    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+    res.status(StatusCodes.OK).json({ user: { name: user.username }, token });
   } catch (error) {
     res.status(StatusCodes.UNAUTHORIZED).json({ error: error.message });
   }

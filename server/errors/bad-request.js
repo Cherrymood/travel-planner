f27410e64860a376249812647230b664350d8c16
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2025-02-26 21:57:25 -0800

    Revert "Revert "Tests (#18)" (#19)" (#20)
    This reverts commit 1477fd50862f4c219617cdfecd500c0937a961b7.

--- server/errors/bad-request.js
+++ server/errors/bad-request.js
@@ -1,9 +1,11 @@
 import CustomAPIError from "./custom-api.js";
+import { StatusCodes } from 'http-status-codes';
 
 class BadRequestError extends CustomAPIError {
   constructor(message) {
     super(message);
-    this.statusCode = 400;
+    this.name = 'BadRequestError';
+    this.statusCode = StatusCodes.BAD_REQUEST;
   }
 }
 
